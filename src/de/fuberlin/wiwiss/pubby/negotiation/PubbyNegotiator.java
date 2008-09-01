aa13d861037b5b877f7735af52396a92bd16966d
Richard Cyganiak <richard@cyganiak.de>
2008-09-01 23:00:22 +0000

    Tweaks to the content negotiation for better compatibility etc

--- src/de/fuberlin/wiwiss/pubby/negotiation/PubbyNegotiator.java
+++ src/de/fuberlin/wiwiss/pubby/negotiation/PubbyNegotiator.java
@@ -2,8 +2,6 @@ package de.fuberlin.wiwiss.pubby.negotiation;
 
 import java.util.regex.Pattern;
 
-
-
 public class PubbyNegotiator {
 	private final static ContentTypeNegotiator pubbyNegotiator;
 	private final static ContentTypeNegotiator dataNegotiator;
@@ -22,32 +20,30 @@ public class PubbyNegotiator {
 		// HTML to MSIE, no matter what. Boy, do I hate IE.
 		pubbyNegotiator.addUserAgentOverride(Pattern.compile("MSIE"), null, "text/html");
 		
-//		pubbyNegotiator.addUserAgentOverride(Pattern.compile("Safari"), "*/*", "text/html");
-
-		pubbyNegotiator.addVariant("text/html;q=0.6")
-				.addAliasMediaType("application/xhtml+xml;q=0.6");
+		pubbyNegotiator.addVariant("text/html;q=0.81")
+				.addAliasMediaType("application/xhtml+xml;q=0.81");
 		pubbyNegotiator.addVariant("application/rdf+xml")
-				.addAliasMediaType("application/xml;q=0.5")
+				.addAliasMediaType("application/xml;q=0.45")
 				.addAliasMediaType("text/xml;q=0.4");
-		pubbyNegotiator.addVariant("text/rdf+n3;charset=utf-8;q=0.9")
+		pubbyNegotiator.addVariant("text/rdf+n3;charset=utf-8;q=0.95")
 				.addAliasMediaType("text/n3;q=0.5")
 				.addAliasMediaType("application/n3;q=0.5");
-		pubbyNegotiator.addVariant("application/x-turtle;q=0.9")
+		pubbyNegotiator.addVariant("application/x-turtle;q=0.95")
 				.addAliasMediaType("application/turtle;q=0.8")
 				.addAliasMediaType("text/turtle;q=0.5");
-		pubbyNegotiator.addVariant("text/plain;q=0.59");
+		pubbyNegotiator.addVariant("text/plain;q=0.2");
 
 		dataNegotiator = new ContentTypeNegotiator();
-		dataNegotiator.addVariant("application/rdf+xml")
-				.addAliasMediaType("application/xml;q=0.5")
+		dataNegotiator.addVariant("application/rdf+xml;q=0.99")
+				.addAliasMediaType("application/xml;q=0.45")
 				.addAliasMediaType("text/xml;q=0.4");
 		dataNegotiator.addVariant("text/rdf+n3;charset=utf-8")
 				.addAliasMediaType("text/n3;q=0.5")
 				.addAliasMediaType("application/n3;q=0.5");
-		dataNegotiator.addVariant("application/x-turtle")
+		dataNegotiator.addVariant("application/x-turtle;q=0.99")
 				.addAliasMediaType("application/turtle;q=0.8")
 				.addAliasMediaType("text/turtle;q=0.5");
-		dataNegotiator.addVariant("text/plain;q=0.8");
+		dataNegotiator.addVariant("text/plain;q=0.2");
 	}
 	
 	public static ContentTypeNegotiator getPubbyNegotiator() {
--- test/de/fuberlin/wiwiss/pubby/negotiation/PubbyNegotiatorTest.java
+++ test/de/fuberlin/wiwiss/pubby/negotiation/PubbyNegotiatorTest.java
@@ -79,4 +79,42 @@ public class PubbyNegotiatorTest extends TestCase {
 	public void testAcceptEverythingGetsHTML() {
 		assertEquals("text/html", negotiator.getBestMatch("*/*").getMediaType());
 	}
+	
+	public void testFirefox3GetsHTML() {
+		// Firefox 3.0.1, OS X
+		assertEquals("text/html",
+				negotiator.getBestMatch(
+						"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+						"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.1) " + 
+						"Gecko/2008070206 Firefox/3.0.1").getMediaType());
+	}
+	
+	public void testTabulatorGetsRDF() {
+		// Tabulator 0.8.5 on Firefox 3.0.1, OS X
+		assertEquals("application/rdf+xml",
+				negotiator.getBestMatch(
+						"application/rdf+xml, application/xhtml+xml;q=0.3, text/xml;q=0.2, " +
+						"application/xml;q=0.2, text/html;q=0.3, text/plain;q=0.1, text/n3, " +
+						"text/rdf+n3;q=0.5, application/x-turtle;q=0.2, text/turtle;q=1",
+						"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.1) " +
+						"Gecko/2008070206 Firefox/3.0.1").getMediaType());
+	}
+	
+	public void testDataURIDefaultsToN3ForFirefox() {
+		assertEquals("text/rdf+n3;charset=utf-8",
+				PubbyNegotiator.getDataNegotiator().getBestMatch(
+						"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+				).getMediaType());
+	}
+	
+	public void testDataURIDefaultsToN3ForSafari() {
+		assertEquals("text/rdf+n3;charset=utf-8",
+				PubbyNegotiator.getDataNegotiator().getBestMatch(
+						"text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,"+
+						"text/plain;q=0.8,image/png,*/*;q=0.5",
+						"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_4; en-us) " +
+						"AppleWebKit/525.18 (KHTML, like Gecko) " +
+						"Version/3.1.2 Safari/525.20.1"
+				).getMediaType());
+	}
 }

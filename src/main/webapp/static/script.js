36b50ca50f8a9fb583fcca70ec11950b92a57ba7
Richard Cyganiak <richard@cyganiak.de>
2013-10-26 18:26:24 +0100

    Improved JS using jQuery

--- src/main/webapp/static/script.js
+++ src/main/webapp/static/script.js
@@ -1,47 +1,25 @@
 $(function() {
-    init_long_literals();
+    // Truncate long literals with a 'more' expander
+    $('span.literal .lex').each(function(i, lex) {
+        var match = $(lex).text().match(/([^\0]{250}[^\0]*? )([^\0]*)/);
+        if (!match) return;
+        $(lex).hide().after('<span class="lex-truncated">' + match[1] +
+                ' ... <a href="#" class="expander">more</a></span>')
+            .next().find('.expander').click(function() {
+                $(lex).show().next().remove();
+            });
+    });
+    // Button that expands all metadata tables
     $('.expand-meta-all').click(function() {
-        $('.metadata-properties').removeClass('collapsed');
-        $('.expand-meta').text('hide');
-        $('.expand-meta-all').addClass('muted');
+        $(this).addClass('muted');
+        $('.metadata-properties').removeClass('collapsed')
+            .find('.expand-meta').text('hide');
         return false;
     });
+    // Buttons above each metadata table that toggle visibility
     $('.expand-meta').click(function() {
-        $(this).closest('.metadata-properties').toggleClass('collapsed');
-        $(this).text($(this).text() == 'more' ? 'hide' : 'more');
+        $(this).text($(this).text() == 'more' ? 'hide' : 'more')
+            .closest('.metadata-properties').toggleClass('collapsed');
         return false;
     });
 });
-
-var long_literal_counter = 0;
-var long_literal_spans = {};
-var long_literal_texts = {};
-function init_long_literals() {
-    var spans = document.getElementsByTagName('span');
-    for (i = 0; i < spans.length; i++) {
-        if (spans[i].className != 'literal') continue;
-        var span = spans[i];
-        var textNode = span.firstChild;
-        var text = textNode.data;
-        if (text.length < 300) continue;
-        var match = text.match(/([^\0]{250}[^\0]*? )([^\0]*)/);
-        if (!match) continue;
-        span.insertBefore(document.createTextNode(match[1] + ' ... '), span.firstChild);
-        span.removeChild(textNode);
-        var link = document.createElement('a');
-        link.href = 'javascript:expand(' + long_literal_counter + ');';
-        link.appendChild(document.createTextNode('more'));
-        link.className = 'expander';
-        span.insertBefore(link, span.firstChild.nextSibling);
-        long_literal_spans[long_literal_counter] = span;
-        long_literal_texts[long_literal_counter] = textNode;
-        long_literal_counter = long_literal_counter + 1;
-    }
-}
-
-function expand(i) {
-    var span = long_literal_spans[i];
-    span.removeChild(span.firstChild);
-    span.removeChild(span.firstChild);
-    span.insertBefore(long_literal_texts[i], span.firstChild);
-}
